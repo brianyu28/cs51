c0315fa81207d889ed1c764203f5422f60d6302f
Brian Yu <brianyu28@gmail.com>
2019-02-07 22:25:06 -0500

    Add code review 1 links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>CS51 Code Review</h1>
+        <h2>with Brian Yu</h2>
+        <hr/>
+        <a className="button-link" href="http://courseboards.com/brian">
+          <div className="button">Live Feedback</div>
+        </a>
+        <a className="button-link" href="https://goo.gl/forms/9n6HXLbNpgFhVsBh1">
+          <div className="button">Topic Requests</div>
+        </a>
+        <a className="button-link" href="https://goo.gl/forms/qgt4YjueWiIHidAb2">
+          <div className="button">Code Review 1 Feedback</div>
+        </a>
       </div>
     );
   }
